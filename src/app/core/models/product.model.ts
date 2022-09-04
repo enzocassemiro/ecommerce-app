36ce258198fd23175ec0898a0262765bb3364ecf
enzocass <enzocass@live.com>
2022-09-04 15:02:46 -0300

    refactor(ProductModel): Change product model field

--- src/app/core/models/product.model.ts
+++ src/app/core/models/product.model.ts
@@ -1,15 +1,17 @@
 export interface Product {
-  id:            number;
-  active:        boolean;
-  name:          string;
-  enterprise:    string;
-  image:         string;
-  quantity:      number;
+  id: number;
+  active: boolean;
+  name: string;
+  enterprise: string;
+  image: string;
+  quantity: number;
   quantity_sell: number;
-  price:         number;
-  description:   string;
-  tags:          string[];
-  stars:         number;
-  stars_count:   number;
+  price: number;
+  promotion: boolean;
+  promotion_percentage: number;
+  description: string;
+  tags: string;
+  stars: number;
+  stars_count: number;
 }
 
