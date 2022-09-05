4d5ce759d4775cba82969abcdac1dddc44097d86
enzocass <enzocass@live.com>
2022-09-05 17:48:10 -0300

    feat(UserModel): Create User Module

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id:            number;
+  name:          string;
+  last_name:     string;
+  adress:        string;
+  adress_number: number;
+  cart:          any[];
+  cart_purchase: any[];
+}
