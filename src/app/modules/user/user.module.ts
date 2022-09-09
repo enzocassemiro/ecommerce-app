fddbfa2187b0b114c2c1d51c746b8c6f01f6155f
enzocass <enzocass@live.com>
2022-09-09 10:29:14 -0300

    feat(UserModule): import some components

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,17 +1,32 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ToastModule } from 'primeng/toast';
 
-import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { CartComponent } from './pages/cart/cart.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { UserRoutingModule } from './user-routing.module';
+
 
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    CartComponent
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    SharedModule,
+    InputNumberModule,
+    FormsModule,
+    ButtonModule,
+    ToastModule,
+    DialogModule
   ]
 })
 export class UserModule { }
