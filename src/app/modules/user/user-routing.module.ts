7c4035293a8f4ed777d5904c954a1cdaadf93a52
enzocass <enzocass@live.com>
2022-09-09 10:28:43 -0300

    feat(UserRoute): Added redirect to CartComponent

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './pages/cart/cart.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent
+  },
+  {
+    path: '',
+    redirectTo: 'cart',
+    pathMatch: 'full'
+  },
+  {
+    path:'cart',
+    component: CartComponent
   }
 ];
 
